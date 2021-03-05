c1dc9823d7b6ca434fb27597518248963a52dd97
Nikita <didrexov@gmail.com>
2021-03-05 14:09:59 +0300

    task02 and test for it

---
+++ homework1/sample_project/tests/test_task02.py
@@ -0,0 +1,11 @@
+import pytest
+from task02 import _check_window
+
+
+@pytest.mark.parametrize(
+    ["x", "y", "z", "expected_result"],
+    [(1, 2, 3, True), (1, 2, 4, False), (0, 0, 0, True)],
+)
+def test__check_window(x, y, z, expected_result):
+    actual_result = _check_window(x, y, z)
+    assert actual_result == expected_result
