0c8a2f13b396ad8d0f496781ec20f38fc0c76b82
Nikita <didrexov@gmail.com>
2021-03-05 17:25:15 +0300

    task05 and test

---
+++ homework1/sample_project/tests/test_task05.py
@@ -0,0 +1,17 @@
+import pytest
+from task05 import find_maximal_subarray_sum
+
+
+@pytest.mark.parametrize(
+    ["list_of_nums", "k", "expected_sum"],
+    [
+        ([10, 10, 10, 10, 10, -100, 20, 15], 3, 35),
+        ([10, -10], 2, 10),
+        ([-10, 10], 2, 10),
+        ([1, 3, -1, -3, 5, 3, 6, 7], 3, 16),
+        ([10], 1, 10),
+    ],
+)
+def test_find_maximal_subarray_sum(list_of_nums: list, k: int, expected_sum: int):
+    actual_sum = find_maximal_subarray_sum(list_of_nums, k)
+    assert actual_sum == expected_sum
